beeb64f30fa13182c17d1b5ef34b43d71d512d65
Juampi Filevich <juan.filevich@fing.edu.uy>
2020-01-11 16:52:47 -0300

    width & height as parametrs

--- canvas.go
+++ canvas.go
@@ -6,12 +6,10 @@ import (
 )
 
 // Canvas .
-type Canvas [][]rune
-
-const (
-	width  = 75
-	height = 19
-)
+type Canvas struct {
+	content       [][]rune
+	width, height int
+}
 
 // Raw .
 func Raw(obj string) string {
@@ -25,7 +23,7 @@ func Replace(this, that, here string) string {
 
 // Debug .
 func (canvas Canvas) Debug(p Point) (rune, string) {
-	var c rune = canvas[p.X][p.Y]
+	var c rune = canvas.content[p.X][p.Y]
 	return c, strconv.QuoteRune(c)
 }
 
@@ -34,9 +32,9 @@ func (canvas Canvas) Render() string {
 	// el string va a tener el mismo tamano que
 	// el canvas + los \n que no estan
 	res := ""
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			res += string(canvas[x][y])
+	for y := 0; y < canvas.height; y++ {
+		for x := 0; x < canvas.width; x++ {
+			res += string(canvas.content[x][y])
 		}
 		res += "\n"
 	}
@@ -59,23 +57,26 @@ func (canvas Canvas) Draw(fromX, fromY int, obj string) {
 			y++
 			x = fromX
 		} else {
-			canvas[x][y] = char
+			canvas.content[x][y] = char
 			x++
 		}
 	}
 }
 
 // NewCanvas crea un canvas vacio
-func NewCanvas() Canvas {
-	var canvas Canvas
-	canvas = make([][]rune, width)
-	for i := range canvas {
-		canvas[i] = make([]rune, height)
+func NewCanvas(width, height int) Canvas {
+	canvas := Canvas{
+		width:  width,
+		height: height,
+	}
+	canvas.content = make([][]rune, canvas.width)
+	for i := range canvas.content {
+		canvas.content[i] = make([]rune, canvas.height)
 	}
 	// cargo con ' '
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			canvas[x][y] = ' '
+	for y := 0; y < canvas.height; y++ {
+		for x := 0; x < canvas.width; x++ {
+			canvas.content[x][y] = ' '
 		}
 	}
 	return canvas
